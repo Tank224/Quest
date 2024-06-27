5dba36365af243963969eb4de9b49e0ab3cb5299
mosihindana <mosihindana@gmail.com>
2024-06-27 16:09:24 +0300

    Скрытие строки состояния

--- app/src/main/java/com/example/quest/MainActivity.java
+++ app/src/main/java/com/example/quest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.quest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View oView = findViewById(R.id.mainLayout);
+        oView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
+
 }
