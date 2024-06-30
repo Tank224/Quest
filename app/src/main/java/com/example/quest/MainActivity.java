e6760e733db29746899a0d35616139f22c143872
Tank224 <tank364573@mail.ru>
2024-06-30 10:52:55 +0300

    Финальное меню

--- app/src/main/java/com/example/quest/MainActivity.java
+++ app/src/main/java/com/example/quest/MainActivity.java
@@ -11,12 +11,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imageStat;
+    private TextView textView2;
     private ImageView clickableImageView2;
     private ImageView anotherSmallImage;
     private ImageView centerImageView;
     private LinearLayout interactionLayout;
     private ImageView buttonImage1;
+    private ImageView clickableAchievements;
     private ImageView buttonImage2;
     private TextView textView;
     private ImageView bigpaperImageView;
@@ -34,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         buttonImage2 = findViewById(R.id.buttonImage2);
         textView = findViewById(R.id.textView);
         bigpaperImageView = findViewById(R.id.bigpaperImageView);
+        textView2 = findViewById(R.id.textView2);
+        imageStat=findViewById(R.id.imageStat);
+        clickableAchievements=findViewById(R.id.clickableAchievements);
 
         anotherSmallImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,12 +57,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSmallImageClick2(View view) {
         anotherSmallImage.setVisibility(View.INVISIBLE);
-        animateImageToCenter("Жизнь состоит из одних вопросов,\nа хочется, чтобы она состояла из одних ответов");
+        animateImageToCenter("Жизнь состоит из одних\n вопросов,а хочется,\n чтобы она состояла \nиз одних ответов");//меняется текст на квесте
+        textView2.setText("Квест 1");
+        imageStat.setImageResource(R.drawable.statuia);//меняется картинка на статуе
     }
 
     public void onAnotherSmallImageClick(View view) {
         clickableImageView2.setVisibility(View.INVISIBLE);
         animateImageToCenter("Жить, как говорится, хорошо!\nА хорошо жить еще лучше!");
+        textView2.setText("Квест 2");
+        imageStat.setImageResource(R.drawable.statuia2);//меняется картинка на статуе
+    }
+    public void onAchievementsClick(View view) {
+        // Обработка нажатия на clickableAchievements
     }
     private void animateImageToCenter(String text) {
         ObjectAnimator moveX = ObjectAnimator.ofFloat(clickableImageView2, "translationX", 0f);
@@ -77,8 +89,10 @@ public class MainActivity extends AppCompatActivity {
                 clickableImageView2.setVisibility(View.VISIBLE);
                 interactionLayout.setVisibility(View.VISIBLE);
                 textView.setVisibility(View.VISIBLE);
+                textView2.setVisibility(View.VISIBLE);
                 buttonImage1.setVisibility(View.VISIBLE);
                 buttonImage2.setVisibility(View.VISIBLE);
+                imageStat.setVisibility(View.VISIBLE);
                 clickableImageView2.setClickable(false);
                 bigpaperImageView.setVisibility(View.VISIBLE);
                 textView.setText(text); // Устанавливаем текст в textView
@@ -118,6 +132,8 @@ public class MainActivity extends AppCompatActivity {
                 textView.setVisibility(View.GONE);
                 buttonImage1.setVisibility(View.GONE);
                 buttonImage2.setVisibility(View.GONE);
+                imageStat.setVisibility(View.GONE);
+                textView2.setVisibility(View.GONE);
                 clickableImageView2.setClickable(true);
                 clickableImageView2.setVisibility(View.VISIBLE);
                 anotherSmallImage.setVisibility(View.VISIBLE);
