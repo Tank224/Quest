4ee617b58e1c8b92091e05ae8fbb362df40c08d7
mosihindana <mosihindana@gmail.com>
2024-07-01 20:01:58 +0300

    Создан класс для ачивок(без запоминания состояния)

--- app/src/main/java/com/example/quest/Achievement.java
+++ app/src/main/java/com/example/quest/Achievement.java
@@ -1,6 +1,6 @@
 package com.example.quest;
 
-import android.content.res.Resources;
+
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
@@ -8,13 +8,13 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Layer;
 
 public class Achievement extends AppCompatActivity {
-
-    private ImageView imageView1;
-    private boolean flag = false;
-    private Resources r;
-    private Drawable[] layers = new Drawable[2];
+    Achive achive1;
+    Achive achive2;
+    Achive achive3;
+    Achive achive4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,30 +23,66 @@ public class Achievement extends AppCompatActivity {
         oView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
-        imageView1 = (ImageView) findViewById(R.id.Achive1);
-        r = getResources();
-        LayerDrawable layerDrawable;
-        layers[0] = r.getDrawable(R.drawable.paper);
-        if (flag == false)
-        {
-            layers[1] = r.getDrawable(R.drawable.corona_black);
-        }
-        else
-        {
-            layers[1] = r.getDrawable(R.drawable.corona);
-        }
-        layerDrawable = new LayerDrawable(layers);
-        imageView1.setImageDrawable(layerDrawable);
+
+        ImageView imageView1 = findViewById(R.id.Achiven1);
+        ImageView imageView2 = findViewById(R.id.Achiven2);
+        ImageView imageView3 = findViewById(R.id.Achiven3);
+        ImageView imageView4 = findViewById(R.id.Achiven4);
+
+        achive1 = new Achive(false, imageView1, R.drawable.corona_black, R.drawable.corona);
+        achive2 = new Achive(false, imageView2, R.drawable.corona_black, R.drawable.corona);
+        achive3 = new Achive(false, imageView3, R.drawable.corona_black, R.drawable.corona);
+        achive4 = new Achive(false, imageView4, R.drawable.corona_black, R.drawable.corona);
+
+        Update(achive1);
+        Update(achive2);
+        Update(achive3);
+        Update(achive4);
+
+
+
     }
+
+    public void Update(Achive achive)
+    {
+       boolean Status = achive.getStatus();
+       ImageView ImVi = achive.getImageView();
+       int picture_do = achive.getIDDO();
+       int picture_bet = achive.getIDBET();
+       if(Status)
+       {
+           ImVi.setBackgroundResource(R.drawable.paper);
+           ImVi.setImageResource(picture_bet);
+       }
+       else
+       {
+           ImVi.setBackgroundResource(R.drawable.paper);
+           ImVi.setImageResource(picture_do);
+       }
+
+    }
+
+
+
     public void onBackClick(View view) {
         finish();
     }
-    public void Change(View view){
-        flag = true;
-        r = getResources();
-        layers[1] = r.getDrawable(R.drawable.corona);
-        LayerDrawable layerDraw = new LayerDrawable(layers);
-        imageView1.setImageDrawable(layerDraw);
+
+    public void Change1(View view){
+        achive1.Status = true;
+        Update(achive1);
     }
 
+    public void Change2(View view){
+        achive2.Status = true;
+        Update(achive2);
+    }
+    public void Change3(View view){
+        achive3.Status = true;
+        Update(achive3);
+    }
+    public void Change4(View view){
+        achive4.Status = true;
+        Update(achive4);
+    }
 }
