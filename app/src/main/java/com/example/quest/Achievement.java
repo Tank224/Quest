0c26a034db58a168e179304b05d95a368d4138b6
mosihindana <mosihindana@gmail.com>
2024-07-04 15:01:13 +0300

    Готовый экран для ачивок, взаймодействующий через файл, до которого можно дозвониться из любой части проги.

--- app/src/main/java/com/example/quest/Achievement.java
+++ app/src/main/java/com/example/quest/Achievement.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Arrays;
 
 public class Achievement extends AppCompatActivity {
